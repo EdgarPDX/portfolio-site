03ec3be2b7c3d96d274244f6aeb6304bd5882361
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-12-16 00:31:25 -0800

    created slideshow, Css styling

--- src/assets/gallery/GalleryImages.js
+++ src/assets/gallery/GalleryImages.js
@@ -1,7 +1,13 @@
 import photo1 from './gallery1.jpg'
+import photo2 from './gallery2.jpg'
+import photo3 from './gallery3.jpg'
+import photo4 from './gallery4.jpg'
 
 const galleryImages = [
-    photo1    
+    photo1,
+    photo2,
+    photo3,
+    photo4    
 ]
 
 export default galleryImages;
